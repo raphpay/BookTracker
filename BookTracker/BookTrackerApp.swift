277e92c91fd9ed03cf796eeebbbe130756aa1c96
raphpay <41484808+raphpay@users.noreply.github.com>
2022-06-15 21:25:05 +0200

    Initial Commit

---
+++ BookTracker/BookTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookTrackerApp.swift
+//  BookTracker
+//
+//  Created by Raphaël Payet on 15/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct BookTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
