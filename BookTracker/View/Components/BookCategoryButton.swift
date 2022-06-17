41524a39b6b9daccec284ec77b6790589604c6d9
raphpay <41484808+raphpay@users.noreply.github.com>
2022-06-17 14:08:03 +0200

    Feature: Handle drag gesture on categories #8

--- BookTracker/View/Components/BookCategoryButton.swift
+++ BookTracker/View/Components/BookCategoryButton.swift
@@ -13,39 +13,22 @@ struct BookCategoryButton: View {
     let tag: Int
     let width: CGFloat
     @Binding var selectedTag: Int
-    
-    var offset: CGFloat {
-        switch selectedTag {
-        case 0:
-            return width / 3 + 16
-        case 1:
-            return 0
-        case 2:
-            return -width / 3 - 16
-        default:
-            return 0;
-        }
-    }
+    var action: (() -> Void)
     
     var body: some View {
-        Button {
-            withAnimation {
-                selectedTag = tag
+        Text(title)
+            .font(.title2)
+            .foregroundColor(selectedTag == tag ? .primary : .secondary)
+            .fontWeight(selectedTag == tag ? .bold : .regular)
+            .onTapGesture {
+                action()
             }
-        } label: {
-            Text(title)
-                .font(.title2)
-                .foregroundColor(selectedTag == tag ? .primary : .secondary)
-                .fontWeight(selectedTag == tag ? .bold : .regular)
-        }
-        .offset(x: offset)
-        .animation(.spring(), value: offset)
     }
 }
 
 
 struct BookCategoryButton_Previews: PreviewProvider {
     static var previews: some View {
-        BookCategoryButton(title: "Reading", tag: 1, width: 400, selectedTag: .constant(1))
+        BookCategoryButton(title: "Reading", tag: 1, width: 400, selectedTag: .constant(1)) {}
     }
 }
--- BookTracker/View/Screens/HomeView.swift
+++ BookTracker/View/Screens/HomeView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct HomeView: View {
     
     @State var selectedTag: Int = 1
+    @State var dragOffset : CGFloat = .zero
     
     var body: some View {
         VStack {
@@ -17,21 +18,84 @@ struct HomeView: View {
                 HStack {
                     BookCategoryButton(title: "To read", tag: 0,
                                        width: geometry.size.width,
-                                       selectedTag: $selectedTag)
+                                       selectedTag: $selectedTag) {
+                        changeTag(tag: 0, width: geometry.size.width)
+                    }
                     Spacer()
                     BookCategoryButton(title: "Reading", tag: 1,
                                        width: geometry.size.width,
-                                       selectedTag: $selectedTag)
+                                       selectedTag: $selectedTag) {
+                        changeTag(tag: 1, width: geometry.size.width)
+                    }
                     Spacer()
                     BookCategoryButton(title: "Finished", tag: 2,
                                        width: geometry.size.width,
-                                       selectedTag: $selectedTag)
+                                       selectedTag: $selectedTag) {
+                        changeTag(tag: 2, width: geometry.size.width)
+                    }
                 }
+                .padding(.top)
+                .padding(.horizontal)
+                .offset(x: dragOffset)
+                .gesture(
+                    DragGesture()
+                        .onChanged { value in
+                            dragOffset = value.translation.width
+                        }
+                        .onEnded { value in
+                            dragGestureEnded(xTranslation: value.translation.width,
+                                             screenWidth: geometry.size.width)
+                        }
+                )
             }
-            .padding(.top)
-            .padding(.horizontal)
             
             Spacer()
+        }
+    }
+    
+    func changeTag(tag: Int, width: CGFloat) {
+        withAnimation {
+            selectedTag = tag
+            changeOffset(width: width)
+        }
+    }
+    
+    func changeOffset(width: CGFloat) {
+        withAnimation {
+            switch selectedTag {
+            case 0:
+                dragOffset = width / 3 + 16
+            case 1:
+                dragOffset = 0
+            case 2:
+                dragOffset = -width / 3 - 16
+            default:
+                dragOffset = 0;
+            }
+        }
+    }
+    
+    func dragGestureEnded(xTranslation: CGFloat, screenWidth: CGFloat) {
+        withAnimation(.spring()) {
+            guard abs(xTranslation) > screenWidth/3 else {
+                return
+            }
+            
+            if xTranslation > 0 {
+                if selectedTag >= 0 {
+                    changeTag(tag: selectedTag - 1, width: screenWidth)
+                } else {
+                    changeTag(tag: 0, width: screenWidth)
+                }
+                changeOffset(width: screenWidth)
+            } else if xTranslation < 0 {
+                if selectedTag <= 2 {
+                    changeTag(tag: selectedTag + 1, width: screenWidth)
+                } else {
+                    changeTag(tag: 2, width: screenWidth)
+                }
+                changeOffset(width: screenWidth)
+            }
         }
     }
 }
