553e86664e676156fcc737be940823ee8a176346
raphpay <41484808+raphpay@users.noreply.github.com>
2022-06-17 13:18:31 +0200

    Feature: Create components for tab bar #8

--- BookTracker/View/Components/TabBar.swift
+++ BookTracker/View/Components/TabBar.swift
@@ -8,17 +8,41 @@
 import SwiftUI
 
 struct TabBar: View {
+    
+    @StateObject var viewRouter: ViewRouter
+    let geometryWidth, geometryHeight: CGFloat
+    
     var body: some View {
-        VStack {
-            Spacer()
+        HStack {
+            TabBarIcon(viewRouter: viewRouter, assignedPage: .home,
+                width: geometryWidth/3, height: geometryHeight/28,
+                       icon: SFSymbols.home.rawValue, title: "Home")
             
-            Text("Home")
+            ZStack {
+                Circle()
+                    .foregroundColor(.white)
+                    .frame(width: geometryWidth/7, height: geometryWidth/7)
+                    .shadow(radius: 4)
+                
+                Image(systemName: SFSymbols.stopwatch.rawValue)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: (geometryWidth/7)-18 , height: (geometryWidth/7)-18)
+                    .foregroundColor(.ui.darkBlueGray)
+            }
+            .offset(y: -geometryHeight/8/2)
+            
+            TabBarIcon(viewRouter: viewRouter, assignedPage: .more,
+                       width: geometryWidth/3, height: geometryHeight/28,
+                       icon: SFSymbols.more.rawValue, title: "More")
         }
+        .frame(width: geometryWidth, height: geometryHeight/8)
+        .background(Color.black.opacity(0.2).shadow(radius: 2))
     }
 }
 
 struct TabBar_Previews: PreviewProvider {
     static var previews: some View {
-        TabBar()
+        TabBar(viewRouter: ViewRouter(), geometryWidth: 400, geometryHeight: 600)
     }
 }
